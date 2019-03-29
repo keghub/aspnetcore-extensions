88d11813b2095f42ff7cf15d11688f486e1d49a9
Renato Golia <renato.golia@educations.com>
2019-03-29 13:03:59 +0100

    Add error filter code and tests

---
+++ tests/Filters/Tests.Error/SetupExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using AutoFixture.Idioms;
+using EMG.Extensions.AspNetCore;
+using EMG.Extensions.AspNetCore.Filters;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+// ReSharper disable InvokeAsExtensionMethod
+
+namespace Tests
+{
+    [TestFixture]
+    public class SetupExtensionsTests
+    {
+        [Test, AutoCustomData]
+        public void AddExceptionHandlerFilter_is_guarded_against_nulls(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(SetupExtensions).GetMethod(nameof(SetupExtensions.AddExceptionHandlerFilter)));
+        }
+
+        [Test, AutoCustomData]
+        public void AddExceptionHandlerFilter_adds_filter_to_MvcOptions(MvcOptions options)
+        {
+            SetupExtensions.AddExceptionHandlerFilter(options);
+
+            Assert.That(options.Filters.Any(i => i is TypeFilterAttribute typeFilter && typeFilter.ImplementationType == typeof(ExceptionHandlerFilter)));
+        }
+    }
+}
