829b63f5de266bc27cb55f4798bce35963c95738
Renato Golia <renato.golia@educations.com>
2019-04-03 23:44:21 +0200

    Add integration tests

---
+++ tests/Authentication/Tests.Jwt.Web/AuthenticationTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AuthenticationTests
+    {
+        [Test, AutoData]
+        public async Task Get_returns_401_if_no_token_is_present(TestWebApplicationFactory factory, string url)
+        {
+            var client = factory.CreateClient();
+
+            var response = await client.GetAsync($"/{url}");
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [Test, AutoData]
+        public async Task Get_returns_200_if_authenticated(TestWebApplicationFactory factory, string url)
+        {
+            var client = factory.CreateClient();
+
+            string token = null;
+
+            using (var authRequest = new HttpRequestMessage(HttpMethod.Post, factory.AuthenticationEndpoint))
+            {
+                authRequest.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    ["username"] = factory.Username,
+                    ["password"] = factory.Password
+                });
+
+                using (var authResponse = await client.SendAsync(authRequest))
+                {
+                    authResponse.EnsureSuccessStatusCode();
+
+                    var body = await authResponse.Content.ReadAsStringAsync();
+
+                    var obj = JObject.Parse(body);
+
+                    token = obj["accessToken"].Value<string>();
+                }
+            }
+
+            Assume.That(token, Is.Not.Null);
+
+            using (var request = new HttpRequestMessage(HttpMethod.Get, $"/{url}"))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("BEARER", token);
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                }
+            }
+        }
+    }
+}
---
+++ tests/Authentication/Tests.Jwt.Web/TestWebApplicationFactory.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using EMG.Extensions.AspNetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests
+{
+    public class TestWebApplicationFactory : WebApplicationFactory<TestStartup>
+    {
+        private string _authenticationEndpoint;
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string AuthenticationEndpoint
+        {
+            get => _authenticationEndpoint;
+            set => _authenticationEndpoint = value.StartsWith("/") ? value : $"/{value}";
+        }
+
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            return WebHost.CreateDefaultBuilder<TestStartup>(new string[0]).ConfigureAppConfiguration(ConfigureConfiguration);
+        }
+
+        private void ConfigureConfiguration(WebHostBuilderContext context, IConfigurationBuilder builder)
+        {
+            var settings = new Dictionary<string, string>
+            {
+                ["JWT:SecretKey"] = "test-key-test-key-test-key",
+                ["JWT:IssuerOptions:Issuer"] = "Test API",
+                ["JWT:IssuerOptions:Audience"] = "https://localhost:5001",
+                ["JWT:Client:Username"] = Username,
+                ["JWT:Client:Password"] = Password,
+                ["JWT:AuthenticationEndpoint"] = AuthenticationEndpoint
+            };
+
+            builder.AddInMemoryCollection(settings);
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseContentRoot(".");
+            base.ConfigureWebHost(builder);
+        }
+    }
+
+    public class TestStartup
+    {
+        public TestStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddJwtAuthentication(Configuration)
+                    .RequireAuthentication()
+                    .AddBasicUserAuthenticator()
+                    .AddFormUserExtractor();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+
+            app.UseJwtAuthentication();
+
+
+            app.UseHttpsRedirection();
+
+            app.UseMvc();
+        }
+
+    }
+
+    [ApiController]
+    public class TestController
+    {
+        [HttpGet("{text}")]
+        public ActionResult<string> Get(string text) => text;
+    }
+}
